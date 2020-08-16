5bb3f5238b46aa32b0a74494720f59d642bca818
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-16 21:47:20 +0530

    colors integrated to the porject

--- MakeLifeStyles/Extensions/UIColor+Ext.swift
+++ MakeLifeStyles/Extensions/UIColor+Ext.swift
@@ -1,7 +1,13 @@
 import UIKit
 
 enum AssertColor: String {
-    case lightGray = "color_name"
+    case lightAsh = "light_ash"
+    case darkestAsh = "darkest_ash"
+    case greenishBlue = "greenish_blue"
+    case lighestGreen = "lighest_green"
+    case lightYellow = "light_yellow"
+    case pinkishRed = "pinkish_red"
+    case teal = "teal"
 }
 
 
