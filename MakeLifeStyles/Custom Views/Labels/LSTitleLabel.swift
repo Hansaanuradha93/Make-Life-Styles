4a93fef3c14753cbf9cb03e410f043b79d76bd15
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-17 11:02:36 +0530

    LSTitleLabel added

---
+++ MakeLifeStyles/Custom Views/Labels/LSTitleLabel.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+class LSTitleLabel: UILabel {
+
+    // MARK: Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    
+    convenience init(text: String = "", textColor: UIColor = .black, fontSize: CGFloat = 18, textAlignment: NSTextAlignment = .center, numberOfLines: Int = 1) {
+        self.init(frame: .zero)
+        self.text = text
+        self.textColor = textColor
+        self.font = UIFont(name: "AvenirNext-Bold", size: fontSize)
+        self.textAlignment = textAlignment
+        self.numberOfLines = numberOfLines
+    }
+}
+
+
+// MARK: - Private Methods
+extension LSTitleLabel {
+    
+    private func configure() {
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+    }
+}
