2393ac6c5515c8807b97102dad9c929b5b101bb0
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-17 09:30:52 +0530

    attributed string added to Get Started screen

--- MakeLifeStyles/Scenes/Onboading/Get Started/GetStartedVC.swift
+++ MakeLifeStyles/Scenes/Onboading/Get Started/GetStartedVC.swift
@@ -24,21 +24,20 @@ class GetStartedVC: UIViewController {
         view.addSubview(heroImageView)
         heroImageView.anchor(top: circleImageView.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0), size: .init(width: 0, height: view.frame.height / 2))
         
-        titleLabel.font = UIFont.systemFont(ofSize: 40.0)
-        titleLabel.text = "It's time to build\nsome habits!"
+        titleLabel.attributedText = NSMutableAttributedString().normal("It's time to build\n", 40).bold("some habits!", 40)
         titleLabel.textColor = UIColor.appColor(color: .lightBlack)
         titleLabel.textAlignment = .center
         titleLabel.numberOfLines = 2
         view.addSubview(titleLabel)
         titleLabel.anchor(top: heroImageView.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 20, bottom: 0, right: 20))
         
-        descriptionLabel.font = UIFont.systemFont(ofSize: 16, weight: .light)
+        descriptionLabel.font = UIFont.systemFont(ofSize: 18, weight: .light)
         descriptionLabel.text = "Are you ready?\nNow we gonna build your\n morning routine!"
         descriptionLabel.numberOfLines = 3
         descriptionLabel.textColor = UIColor.appColor(color: .greenishBlue)
         descriptionLabel.textAlignment = .center
         view.addSubview(descriptionLabel)
-        descriptionLabel.anchor(top: titleLabel.bottomAnchor, leading: titleLabel.leadingAnchor, bottom: nil, trailing: titleLabel.trailingAnchor, padding: .init(top: 20, left: 0, bottom: 0, right: 0))
+        descriptionLabel.anchor(top: titleLabel.bottomAnchor, leading: titleLabel.leadingAnchor, bottom: nil, trailing: titleLabel.trailingAnchor, padding: .init(top: 15, left: 0, bottom: 0, right: 0))
         
         callToActionButton.setTitle("Continue", for: .normal)
         callToActionButton.setTitleColor(.white, for: .normal)
@@ -46,6 +45,6 @@ class GetStartedVC: UIViewController {
         callToActionButton.backgroundColor = UIColor.appColor(color: .lightBlack)
         callToActionButton.layer.cornerRadius = 65 / 2
         view.addSubview(callToActionButton)
-        callToActionButton.anchor(top: nil, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 20, bottom: 20, right: 20), size: .init(width: 0, height: 65))
+        callToActionButton.anchor(top: nil, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 20, bottom: 10, right: 20), size: .init(width: 0, height: 65))
     }
 }
