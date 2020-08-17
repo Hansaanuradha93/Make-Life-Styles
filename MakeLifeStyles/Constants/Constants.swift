df62a0a2d6edcadb884f1a0b65dec386bcd0858e
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-17 11:44:56 +0530

    Constants added

---
+++ MakeLifeStyles/Constants/Constants.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+struct Asserts {
+    static let placeHolder = UIImage(named: "")!
+    static let personOnBicycle = UIImage(named: "person_on_bicycle")!
+    static let circle = UIImage(named: "circle")!
+}
+
+
+struct Strings {
+    static let areYouReady = "Are you ready?\nNow we gonna build your\n morning routine!"
+    static let continueString = "Continue"
+    static let itsTimeToBuild = "It's time to build\n"
+    static let someHabits = "some habits!"
+}
