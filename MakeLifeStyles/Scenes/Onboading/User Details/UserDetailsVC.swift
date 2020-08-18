bf4bd1945098d1e83265b042b6d805ee6ec5ecee
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-18 22:28:04 +0530

    UserDetails screen hero section designed

--- MakeLifeStyles/Scenes/Onboading/User Details/UserDetailsVC.swift
+++ MakeLifeStyles/Scenes/Onboading/User Details/UserDetailsVC.swift
@@ -4,9 +4,15 @@ class UserDetailsVC: UIViewController {
 
     let heroImageContainer = UIView()
     let detailsContainer = UIView()
+    let circleImageView = LSImageView(image: Asserts.circle)
+    let heroImageView = LSImageView(image: Asserts.personOnScooter)
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.navigationBar.isHidden = true // remove this line
+
         
         heroImageContainer.backgroundColor = UIColor.appColor(color: .lightYellow)
         view.addSubview(heroImageContainer)
@@ -15,5 +21,12 @@ class UserDetailsVC: UIViewController {
         detailsContainer.backgroundColor = UIColor.appColor(color: .darkestAsh)
         view.addSubview(detailsContainer)
         detailsContainer.anchor(top: view.centerYAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+        
+        heroImageContainer.addSubview(circleImageView)
+        circleImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: nil, size: .init(width: 35, height: 35))
+        circleImageView.centerHorizontallyInSuperView()
+        
+        heroImageContainer.addSubview(heroImageView)
+        heroImageView.anchor(top: circleImageView.bottomAnchor, leading: view.leadingAnchor, bottom: heroImageContainer.bottomAnchor, trailing: view.trailingAnchor)
     }
 }
