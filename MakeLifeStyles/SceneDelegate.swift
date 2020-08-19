54d151cf75022a6699cb61bc22f8a9ca8c379c0d
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-19 22:11:04 +0530

    User details screen refactored

--- MakeLifeStyles/SceneDelegate.swift
+++ MakeLifeStyles/SceneDelegate.swift
@@ -7,7 +7,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let controller = UINavigationController(rootViewController: UserDetailsVC())
+        let controller = UINavigationController(rootViewController: GetStartedVC())
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = controller
         self.window = window
