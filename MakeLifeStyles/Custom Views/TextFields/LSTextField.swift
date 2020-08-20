91936323adc9a09e43bfbe98adddd712d50bd157
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-20 22:49:23 +0530

    LSTextField added and designed

---
+++ MakeLifeStyles/Custom Views/TextFields/LSTextField.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+class LSTextField: UITextField {
+    
+    // MARK: Properties
+    fileprivate var padding: CGFloat = 0
+    
+    
+    // MARK: Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    convenience init(backgroundColor: UIColor = .white, textColor: UIColor = .black, textSize: CGFloat, borderStyle: UITextField.BorderStyle = .line, padding: CGFloat = 0, placeholderText: String = "") {
+        self.init()
+        self.backgroundColor = backgroundColor
+        self.textColor = textColor
+        self.font = UIFont(name: "AvenirNext-Regular", size: textSize)
+        self.borderStyle = borderStyle
+        self.padding = padding
+        self.placeholder = placeholderText
+    }
+    
+    
+    required init?(coder: NSCoder) { fatalError() }
+
+    
+    // MARK: Overridden Methods
+    override func editingRect(forBounds bounds: CGRect) -> CGRect { return bounds.insetBy(dx: padding, dy: 0) }
+    
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect { return bounds.insetBy(dx: padding, dy: 0) }
+}
+
+
+// MARK: - Methods
+extension LSTextField {
+    func setRoundedBorder(borderColor: UIColor, borderWidth: CGFloat, radius: CGFloat) {
+        self.layer.borderColor = borderColor.cgColor
+        self.layer.borderWidth = borderWidth
+        self.layer.cornerRadius = radius
+        self.clipsToBounds = true
+    }
+}
+
