e7fffd9feb9a1f4f8a841c3027675563963ccb88
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-21 22:56:11 +0530

    CoreData added

--- MakeLifeStyles/AppDelegate.swift
+++ MakeLifeStyles/AppDelegate.swift
@@ -1,9 +1,41 @@
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: Properties
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Data")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+
+    // MARK: Methods
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
+    }
+}
+
+
+// MARK: - Core Data Saving support
+extension AppDelegate {
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 }
 
