f6532ce61fb614447d58085039bfc0ac8a766709
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-22 22:24:36 +0530

    LSTab bar tabs added

--- MakeLifeStyles/Custom Views/Tab  Bar/LSTabBar.swift
+++ MakeLifeStyles/Custom Views/Tab  Bar/LSTabBar.swift
@@ -2,7 +2,31 @@ import UIKit
 
 class LSTabBar: UITabBarController {
 
+    // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
+//        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createHomeNC(), createFavouriteListNC()]
+    }
+}
+
+
+// MARK: - Methods
+extension LSTabBar {
+    
+    fileprivate func createHomeNC() -> UINavigationController {
+        let searchVC = HomeVC()
+        searchVC.title = "Home"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    
+    fileprivate func createFavouriteListNC() -> UINavigationController {
+        let favouriteListVC = UIViewController()
+        favouriteListVC.title           = "Favourites"
+        favouriteListVC.tabBarItem      = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favouriteListVC)
     }
 }
