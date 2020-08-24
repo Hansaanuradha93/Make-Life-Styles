22de53b9315eab273e9276cf977e81fb898c3302
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-24 23:17:14 +0530

    HomeVC code style maintained

--- MakeLifeStyles/Scenes/Home/HomeVC.swift
+++ MakeLifeStyles/Scenes/Home/HomeVC.swift
@@ -1,21 +1,32 @@
 import UIKit
 
-class HomeVC: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class HomeVC: UICollectionViewController {
 
+    // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
     }
+}
+
+
+// MARK: - Collection View
+extension HomeVC {
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 6
+        return 4
     }
+    
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HabbitCell.reuseID, for: indexPath) as! HabbitCell
         return cell
     }
-    
+}
+
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension HomeVC {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let dimenstions = (collectionView.frame.width - 10) / 2
