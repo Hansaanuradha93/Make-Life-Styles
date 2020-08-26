9480617e320142fe2a4820686977a35ad015f95b
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-26 22:40:12 +0530

    added rings to the HabitCell

--- MakeLifeStyles/Scenes/Home/Cells/HabbitCell.swift
+++ MakeLifeStyles/Scenes/Home/Cells/HabbitCell.swift
@@ -14,7 +14,11 @@ class HabbitCell: UICollectionViewCell {
     // MARK: Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .blue
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
         addRings()
     }
     
@@ -27,23 +31,19 @@ class HabbitCell: UICollectionViewCell {
 extension HabbitCell {
     
     fileprivate func addRings() {
-        let containerView = UIView()
-        containerView.backgroundColor = .yellow
-        addSubview(containerView)
-        containerView.fillSuperview()
-        
-        
-        radius = containerView.frame.width / 2 - 30
+        backgroundColor = .yellow
+        radius = frame.width / 2 - 30
         trackRing = addRing(radius: radius, strokeColor:  UIColor.appColor(color: .lightBlack), fillColor: .clear)
         shapeRing = addRing(radius: radius, strokeColor: UIColor.appColor(color: .pinkishRed), fillColor: .clear, strokeEnd: 0.5)
         
-        containerView.layer.addSublayer(trackRing)
-        containerView.layer.addSublayer(shapeRing)
-
+        layer.addSublayer(trackRing)
+        layer.addSublayer(shapeRing)
     }
     
     
     fileprivate func addRing(radius: CGFloat, strokeColor: UIColor, fillColor: UIColor, lineWidth: CGFloat = 10, strokeEnd: CGFloat = 1) -> CAShapeLayer {
+        let center = CGPoint(x: bounds.midX, y: bounds.midY - 20
+        )
         let ring = CAShapeLayer()
          
         let circularPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi - CGFloat.pi / 2, clockwise: true)
