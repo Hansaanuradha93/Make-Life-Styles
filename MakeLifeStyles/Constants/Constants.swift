992df320319d4f0565c0718f9f4476ae8c1d8b87
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-11-18 17:24:08 +0530

    Constants code cleaned

--- MakeLifeStyles/Constants/Constants.swift
+++ MakeLifeStyles/Constants/Constants.swift
@@ -1,25 +1,36 @@
 import UIKit
 
 struct Asserts {
-    static let placeHolder = UIImage(named: "placeholder")!
-    static let personOnBicycle = UIImage(named: "person_on_bicycle")!
-    static let circle = UIImage(named: "circle")!
-    static let personOnScooter = UIImage(named: "person_on_scooter")!
+    // Tab Bar
     static let add = UIImage(named: "add")!
     static let home = UIImage(named: "home")!
     static let star = UIImage(named: "star")!
-
+    
+    // Screens
+    static let personOnBicycle = UIImage(named: "person_on_bicycle")!
+    static let circle = UIImage(named: "circle")!
+    static let personOnScooter = UIImage(named: "person_on_scooter")!
+    
+    // Common
+    static let placeHolder = UIImage(named: "placeholder")!
 }
 
 
 struct Strings {
-    static let empty = ""
+    // Get Started Screen
     static let areYouReady = "Are you ready?\nNow we gonna build your\n morning routine!"
-    static let continueString = "Continue"
     static let itsTimeToBuild = "It's time to build\n"
     static let someHabits = "some habits!"
-    static let back = "Back"
+    
+    // User Details Screen
     static let one = "1."
     static let nameQuestion = "Okay firstly,\n how can I call you?"
     static let enterYourName = "Enter your name"
+    
+    // Common
+    static let empty = ""
+
+    // Buttons
+    static let back = "Back"
+    static let continueString = "Continue"
 }
