c34d1d6138fcdc1427af9c28b15396720479bb36
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-11-21 22:36:23 +0530

    HomeVM added added and fetched habits

---
+++ MakeLifeStyles/Scenes/Home/HomeVM.swift
@@ -0,0 +1,26 @@
+import UIKit
+import CoreData
+
+final class HomeVM {
+    
+    // MARK: Properties
+    var habits = [Habit]()
+    
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+}
+
+
+// MARK: Public Methods
+extension HomeVM {
+    
+    func fetchHabits(completion: @escaping (Bool) -> ()) {
+        do {
+            let habits = try context.fetch(Habit.fetchRequest())
+            self.habits = habits as! [Habit]
+            completion(true)
+        } catch let error as NSError {
+            print("Could not fetch data. \(error), \(error.userInfo)")
+            completion(false)
+        }
+    }
+}
