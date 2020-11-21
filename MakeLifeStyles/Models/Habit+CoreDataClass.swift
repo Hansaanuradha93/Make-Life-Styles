612d05b897391b5222bc1602be79cd39dd0b8e83
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-11-21 23:36:33 +0530

    CoreData Habit entity changed

--- MakeLifeStyles/Models/Habit+CoreDataClass.swift
+++ MakeLifeStyles/Models/Habit+CoreDataClass.swift
@@ -1,12 +1,3 @@
-//
-//  Habit+CoreDataClass.swift
-//  MakeLifeStyles
-//
-//  Created by Hansa Anuradha on 2020-11-20.
-//  Copyright © 2020 Hansa Anuradha. All rights reserved.
-//
-//
-
 import Foundation
 import CoreData
 
--- MakeLifeStyles/Models/Habit+CoreDataProperties.swift
+++ MakeLifeStyles/Models/Habit+CoreDataProperties.swift
@@ -1,12 +1,3 @@
-//
-//  Habit+CoreDataProperties.swift
-//  MakeLifeStyles
-//
-//  Created by Hansa Anuradha on 2020-11-20.
-//  Copyright © 2020 Hansa Anuradha. All rights reserved.
-//
-//
-
 import Foundation
 import CoreData
 
@@ -17,14 +8,13 @@ extension Habit {
         return NSFetchRequest<Habit>(entityName: "Habit")
     }
 
+    @NSManaged public var category: String?
+    @NSManaged public var days: Int16
     @NSManaged public var initialDays: Int16
     @NSManaged public var name: String?
     @NSManaged public var repetitions: Int16
     @NSManaged public var startDate: Date?
     @NSManaged public var type: String?
-    @NSManaged public var id: Int32
-    @NSManaged public var category: String?
-    @NSManaged public var days: Int16
 
 }
 
