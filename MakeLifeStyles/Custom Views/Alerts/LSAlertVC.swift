7f0114f27772ebeb726c7b0d6d44aa737447532d
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-11-22 21:41:59 +0530

    LSAlertVC code refactored

--- MakeLifeStyles/Custom Views/Alerts/LSAlertVC.swift
+++ MakeLifeStyles/Custom Views/Alerts/LSAlertVC.swift
@@ -6,7 +6,7 @@ class LSAlertVC: UIViewController {
     private let containerView = LSAlertContainerView()
     private let titleLabel = LSTitleLabel(fontSize: 20, textAlignment: .center)
     private let messageLabel = LSBodyLabel(textAlignment: .center)
-    private let actionButton = LSButton(backgroundColor: UIColor.appColor(color: .lightBlack), title: "Ok", titleColor: .white, radius: 25)
+    private let actionButton = LSButton(backgroundColor: UIColor.appColor(color: .lightBlack), titleColor: .white, radius: GlobalDimensions.cornerRadius)
     
     private var alertTitle: String?
     private var message: String?
@@ -44,18 +44,17 @@ private extension LSAlertVC {
     
     func configureUI() {
         view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
-        titleLabel.text = alertTitle ?? "Something went wrong!"
-        actionButton.setTitle(buttonTitle ?? "Ok", for: .normal)
+        titleLabel.text = alertTitle ?? Strings.somethingWentWrong
+        actionButton.setTitle(buttonTitle ?? Strings.ok, for: .normal)
         actionButton.addTarget(self, action: #selector(dismissAlert), for: .touchUpInside)
-        messageLabel.text = message ?? "Unable to complete request"
+        messageLabel.text = message ?? Strings.unableToCompleteRequest
         messageLabel.numberOfLines = 4
 
         view.addSubviews(containerView, titleLabel, messageLabel, actionButton)
         
         containerView.centerInSuperview(size: .init(width: 280, height: 230))
         titleLabel.anchor(top: containerView.topAnchor, leading: containerView.leadingAnchor, bottom: nil, trailing: containerView.trailingAnchor, padding: .init(top: padding, left: padding, bottom: 0, right: padding), size: .init(width: 0, height: 28))
-        actionButton.anchor(top: nil, leading: containerView.leadingAnchor, bottom: containerView.bottomAnchor, trailing: containerView.trailingAnchor, padding: .init(top: 0, left: padding, bottom: padding, right: padding), size: .init(width: 0, height: 50))
+        actionButton.anchor(top: nil, leading: containerView.leadingAnchor, bottom: containerView.bottomAnchor, trailing: containerView.trailingAnchor, padding: .init(top: 0, left: padding, bottom: padding, right: padding), size: .init(width: 0, height: GlobalDimensions.height))
         messageLabel.anchor(top: titleLabel.bottomAnchor, leading: containerView.leadingAnchor, bottom: actionButton.topAnchor, trailing: containerView.trailingAnchor, padding: .init(top: 8, left: padding, bottom: 12, right: padding))
-        
     }
 }
