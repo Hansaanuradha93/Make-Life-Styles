236ecfb519e4ba3d44f9773a22e5dd1a911b393b
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-11-22 15:30:28 +0530

    Core data model changed

--- MakeLifeStyles/Models/Habit+CoreDataProperties.swift
+++ MakeLifeStyles/Models/Habit+CoreDataProperties.swift
@@ -9,12 +9,13 @@ extension Habit {
     }
 
     @NSManaged public var category: String?
-    @NSManaged public var days: Int16
-    @NSManaged public var initialDays: Int16
+    @NSManaged public var days: Int64
+    @NSManaged public var initialDays: Int64
     @NSManaged public var name: String?
-    @NSManaged public var repetitions: Int16
+    @NSManaged public var repetitions: Int64
     @NSManaged public var startDate: Date?
     @NSManaged public var type: String?
+    @NSManaged public var id: UUID?
 
 }
 
