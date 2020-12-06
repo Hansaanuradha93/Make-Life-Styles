f2eb0bb5db257a48c1eb97077cbedf64d10364d9
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-12-06 19:16:14 +0530

    custom button font refactored

--- MakeLifeStyles/Custom Views/Buttons/LSButton.swift
+++ MakeLifeStyles/Custom Views/Buttons/LSButton.swift
@@ -15,13 +15,18 @@ class LSButton: UIButton {
     
     convenience init(backgroundColor: UIColor = .white, title: String = "", titleColor: UIColor = .black, radius: CGFloat = 0, fontSize: CGFloat = 16) {
         self.init(frame: .zero)
+        
+        let traits = [UIFontDescriptor.TraitKey.weight: UIFont.Weight.regular]
+        var descriptor = UIFontDescriptor(fontAttributes: [UIFontDescriptor.AttributeName.family: Fonts.avenirNext])
+        descriptor = descriptor.addingAttributes([UIFontDescriptor.AttributeName.traits: traits])
+        
         self.setTitle(title, for: .normal)
         self.setTitleColor(titleColor, for: .normal)
         self.backgroundColor = backgroundColor
         self.layer.cornerRadius = radius
-        self.titleLabel?.font = UIFont(name: "AvenirNext-Regular", size: fontSize)
         self.layer.masksToBounds = false
         self.clipsToBounds = true
         self.imageView?.contentMode = .scaleAspectFill
+        self.titleLabel?.font = UIFont(descriptor: descriptor, size: fontSize)
     }
 }
