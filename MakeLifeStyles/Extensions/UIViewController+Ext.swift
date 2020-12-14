add7884b5920b367a30ee92fdc9c6519d7d76275
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-12-14 22:18:26 +0530

    Empty State view added to view controller extensions

--- MakeLifeStyles/Extensions/UIViewController+Ext.swift
+++ MakeLifeStyles/Extensions/UIViewController+Ext.swift
@@ -2,6 +2,13 @@ import UIKit
 
 extension UIViewController {
     
+    func showEmptyStateView(with message: String, in view: UIView) {
+        let emptyStateView = LSEmptyStateView(message: message)
+        emptyStateView.frame = view.bounds
+        view.addSubview(emptyStateView)
+    }
+    
+    
     func presentLSAlertOnMainTread(title: String, message: String, buttonTitle: String, action: (() -> Void)? = nil) {
         DispatchQueue.main.async {
             let alertVC = LSAlertVC(title: title, message: message, buttonTitle: buttonTitle, action: action)
