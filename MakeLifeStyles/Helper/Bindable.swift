3a30074ac969abcfd09b2cbc81636c9b25434c8a
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-05-05 20:19:55 +0530

    Bindable code refactored

--- MakeLifeStyles/Helper/Bindable.swift
+++ MakeLifeStyles/Helper/Bindable.swift
@@ -2,13 +2,17 @@ import Foundation
 
 class Bindable<T> {
     
+    // MARK: Properties
     var value: T? {
         didSet { observer?(value) }
     }
     
     
     private var observer: ((T?) -> ())?
-    
+}
+
+// MARK: - Methods
+extension Bindable {
     
     func bind(observer: @escaping (T?) -> ()) {
         self.observer = observer
