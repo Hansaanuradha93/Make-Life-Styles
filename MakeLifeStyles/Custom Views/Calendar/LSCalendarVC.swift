3a6f3761769f1582b92eb73933e2bf057984674b
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-03-03 09:40:18 +0530

    Calendar days generated

--- MakeLifeStyles/Custom Views/Calendar/LSCalendarVC.swift
+++ MakeLifeStyles/Custom Views/Calendar/LSCalendarVC.swift
@@ -2,24 +2,147 @@ import UIKit
 
 class LSCalendarVC: UIViewController {
     
+    // Types
+    enum CalendarDataError: Error {
+        case metadataGeneration
+    }
+    
+    
+    // MARK: UI Properties
     private lazy var collectionView: UICollectionView = {
-      let layout = UICollectionViewFlowLayout()
-      layout.minimumLineSpacing = 0
-      layout.minimumInteritemSpacing = 0
-      
-      let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-      collectionView.isScrollEnabled = false
-//      collectionView.translatesAutoresizingMaskIntoConstraints = false
-      return collectionView
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isScrollEnabled = false
+        return collectionView
     }()
-
+    
+    
+    // MARK: Calendar Date Properties
+    private let selectedDate: Date
+    
+    private var baseDate: Date {
+        didSet {
+            days = generateDaysInMonth(for: baseDate)
+            collectionView.reloadData()
+//            headerView.baseDate = baseDate
+        }
+    }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d"
+        return dateFormatter
+    }()
+    
+    private lazy var days = generateDaysInMonth(for: baseDate)
+    
+    private let selectedDateChanged: ((Date) -> Void)
+    private let calendar = Calendar(identifier: .gregorian)
+    
+    
+    // MARK: Initializers
+    init(baseDate: Date, selectedDateChanged: @escaping ((Date) -> Void)) {
+        self.selectedDate = baseDate
+        self.baseDate = baseDate
+        self.selectedDateChanged = selectedDateChanged
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        collectionView.backgroundColor = .systemGroupedBackground
+        setupUI()
+    }
+}
 
+
+// MARK: - Private Methods
+private extension LSCalendarVC {
+    
+    func setupUI() {
+        collectionView.backgroundColor = .systemGroupedBackground
+        
         view.addSubview(collectionView)
         
         collectionView.fillSuperview()
     }
 }
+
+
+// MARK: - Day Generation
+private extension LSCalendarVC {
+    
+    func monthMetadata(for baseDate: Date) throws -> MonthMetadata {
+        
+        guard let numberOfDaysInMonth = calendar.range(
+            of: .day,
+            in: .month,
+            for: baseDate)?.count,
+              let firstDayOfMonth = calendar.date(
+                from: calendar.dateComponents([.year, .month], from: baseDate)) else {
+                    throw CalendarDataError.metadataGeneration
+                }
+        
+        let firstDayWeekday = calendar.component(.weekday, from: firstDayOfMonth)
+        
+        return MonthMetadata(numberOfDays: numberOfDaysInMonth, firstDay: firstDayOfMonth, firstDayWeekday: firstDayWeekday)
+    }
+    
+    
+    func generateDaysInMonth(for baseDate: Date) -> [Day] {
+        guard let metadata = try? monthMetadata(for: baseDate) else {
+            fatalError("An error occurred when generating the metadata for \(baseDate)")
+        }
+        
+        let numberOfDaysInMonth = metadata.numberOfDays
+        let offsetInInitialRow = metadata.firstDayWeekday
+        let firstDayOfMonth = metadata.firstDay
+        
+        var days: [Day] = (1..<(numberOfDaysInMonth + offsetInInitialRow)).map { day in
+                let isWithinDisplayedMonth = day >= offsetInInitialRow
+                
+                let dayOffset = isWithinDisplayedMonth ? day - offsetInInitialRow : -(offsetInInitialRow - day)
+                
+                return generateDay(offsetBy: dayOffset, for: firstDayOfMonth, isWithinDisplayedMonth: isWithinDisplayedMonth)
+            }
+        
+        days += generateStartOfNextMonth(using: firstDayOfMonth)
+        
+        return days
+    }
+    
+    
+    func generateDay(offsetBy dayOffset: Int, for baseDate: Date, isWithinDisplayedMonth: Bool) -> Day {
+        let date = calendar.date(byAdding: .day, value: dayOffset, to: baseDate) ?? baseDate
+        
+        return Day(date: date, number: dateFormatter.string(from: date), isSelected: calendar.isDate(date, inSameDayAs: selectedDate), isWithinDisplayedMonth: isWithinDisplayedMonth)
+    }
+    
+    
+    func generateStartOfNextMonth(using firstDayOfDisplayedMonth: Date) -> [Day] {
+        guard let lastDayInMonth = calendar.date(byAdding: DateComponents(month: 1, day: -1), to: firstDayOfDisplayedMonth) else { return [] }
+        
+        let additionalDays = 7 - calendar.component(.weekday, from: lastDayInMonth)
+        
+        guard additionalDays > 0 else { return [] }
+        
+        let days: [Day] = (1...additionalDays).map {
+                generateDay(offsetBy: $0, for: lastDayInMonth, isWithinDisplayedMonth: false)
+            }
+        
+        return days
+    }
+
+
+}
+
